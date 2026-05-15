b14018c381402090314285054d91c8fe86d91a5c
cernochmartin <cernochm@outlook.cz>
2026-05-15 14:19:40 +0200

    Initial commit: DokuWiki POS installation with Bootstrap3 template and Czech content (8 sections, 42 pages)

---
+++ conf/local.php
@@ -0,0 +1,13 @@
+<?php
+$conf['title'] = 'Počítačové sítě - POS';
+$conf['lang'] = 'cs';
+$conf['template'] = 'bootstrap3';
+$conf['useheading'] = 1;
+$conf['useacl'] = 0;
+$conf['superuser'] = '';
+$conf['disableactions'] = 'register,resendpwd,revert';
+$conf['savedir'] = './data';
+$conf['remote'] = 0;
+$conf['plugin']['auth']['disabled'] = 1;
+$conf['authtype'] = 'none';
+$conf['tpl']['bootstrap3']['pageInfo'] = 'filename,extension,date,locked';
